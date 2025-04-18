2492ed2134bec4a4c66ec7706e4f136a4bb578b6
e-aub <eaub@proton.me>
2025-04-18 14:22:52 +0100

    added use state

---
+++ core/state.js
@@ -0,0 +1,51 @@
+let states = [];
+let index = 0;
+
+function useState(initialState) {
+    if (typeof states[index] === 'undefined') {
+        if (typeof initialState === 'function') {
+            states[index] = initialState();
+        } else {
+            states[index] = initialState;
+        }
+    }
+    const localIndex = index;
+
+    const setState = (newState) => {
+        if (typeof newState === 'function') {
+            states[localIndex] = newState(states[localIndex]);
+        } else {
+            states[localIndex] = newState;
+        }
+        rerender(); 
+    };
+
+    const state = states[localIndex];
+    index++;
+    return [state, setState];
+}
+
+// function test() {
+//     index = 0; // Reset index to simulate re-render
+//     const [count, setCount] = useState(0);
+//     const persons =  ["john", "jane", "jim", "jill"];
+//     const [person, setPerson] = useState(null);
+//     const button = document.getElementById('count');
+//     const countDisplay = document.getElementById('count-display');
+//     const personDisplay = document.getElementById('person-display');
+//     button.onclick = () => {
+//         setCount((count) => count + 1);
+//         setPerson((person) => persons[count]);
+//     };
+
+//     countDisplay.textContent = `Count: ${count}`;
+//     personDisplay.textContent = `Person: ${person}`;
+// }
+
+// function rerender() {
+//     test(); // Re-run the component
+// }
+
+// test(); // Initial render
+
+export default useState;
